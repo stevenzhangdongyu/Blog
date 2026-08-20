087d402188abd6d156dc670cb9167e1052afe23a
StevenDongyu <stevendongyu@StevenDongyudeMacBook-Air.local>
2026-08-20 19:16:47 +0800

    async picture loading

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,9 +1,21 @@
 
 <script setup lang="ts">
+import { onMounted, ref } from 'vue'
 import ConfettiEffect from './components/ConfettiEffect.vue'
+
+const backgroundReady = ref(false)
+
+onMounted(() => {
+  const background = new Image()
+  background.onload = () => {
+    backgroundReady.value = true
+  }
+  background.src = '/blog-background.jpg'
+})
 </script>
 
 <template>
+  <div class="page-background" :class="{ 'is-ready': backgroundReady }" aria-hidden="true" />
   <ConfettiEffect />
   <RouterView />
 </template>
